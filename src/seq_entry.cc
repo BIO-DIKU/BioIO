e798a9301ae3af6b54e179c8ce427a95cb2c30dd
Andreas Dahl <andreas@hrdahl.dk>
2015-10-23 11:59:54 +0200

    seq_entry fix indentation

--- src/seq_entry.cc
+++ src/seq_entry.cc
@@ -21,31 +21,31 @@
 #include <BioIO/seq_entry.h>
 
 SeqEntry::SeqEntry(SeqEntry::SeqType sequence_type) :
-    type_(sequence_type)
+  type_(sequence_type)
 {}
 
 SeqEntry::SeqEntry(const std::string& name,
                    const std::string& sequence,
                    const std::vector<uint8_t>& scores,
                    SeqEntry::SeqType sequence_type) :
-    name_(name),
-    seq_(sequence),
-    scores_(scores),
-    type_(sequence_type)
+  name_(name),
+  seq_(sequence),
+  scores_(scores),
+  type_(sequence_type)
 {}
 
 SeqEntry::SeqEntry(const SeqEntry& other) :
-    name_(other.name_),
-    seq_(other.seq_),
-    scores_(other.scores_),
-    type_(other.type_)
+  name_(other.name_),
+  seq_(other.seq_),
+  scores_(other.scores_),
+  type_(other.type_)
 {}
 
 SeqEntry::SeqEntry(SeqEntry&& other) :
-    name_(std::move(other.name_)),
-    seq_(std::move(other.seq_)),
-    scores_(std::move(other.scores_)),
-    type_(std::move(other.type_))
+  name_(std::move(other.name_)),
+  seq_(std::move(other.seq_)),
+  scores_(std::move(other.scores_)),
+  type_(std::move(other.type_))
 {}
 
 SeqEntry::~SeqEntry() {
@@ -78,41 +78,41 @@ std::string& SeqEntry::name() {
 }
 
 const std::string& SeqEntry::name() const {
-    return name_;
+  return name_;
 }
 
 std::string& SeqEntry::seq() {
-    return seq_;
+  return seq_;
 }
 
 const std::string& SeqEntry::seq() const {
-    return seq_;
+  return seq_;
 }
 
 std::vector<uint8_t>& SeqEntry::scores() {
-    return scores_;
+  return scores_;
 }
 
 const std::vector<uint8_t>& SeqEntry::scores() const {
-    return scores_;
+  return scores_;
 }
 
 SeqEntry::SeqType SeqEntry::type() const {
-    return type_;
+  return type_;
 }
 
 void SeqEntry::set_name(const std::string& name) {
-    name_ = name;
+  name_ = name;
 }
 
 void SeqEntry::set_seq(const std::string& sequence) {
-    seq_ = sequence;
+  seq_ = sequence;
 }
 
 void SeqEntry::set_scores(const std::vector<uint8_t>& scores) {
-    scores_ = scores;
+  scores_ = scores;
 }
 
 void SeqEntry::set_type(SeqType type) {
-    type_ = type;
+  type_ = type;
 }
