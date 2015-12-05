2ff5b973a8532a8d615bef47ba492ac1e2da7256
Martin Asser Hansen <mail@maasha.dk>
2015-12-05 20:02:50 +0100

    added SeqReader WIP

---
+++ include/BioIO/seq_reader.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2015 BIO-DIKU.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#ifndef BIOIO_SEQ_READER_H_
+#define BIOIO_SEQ_READER_H_
+
+#include <string>
+#include <memory>
+#include <fstream>
+#include <exception>
+#include <iostream>
+
+#include <BioIO/seq_entry.h>
+#include <BioIO/read_buffer.h>
+
+/**
+ * @brief Exception class for SeqReader class.
+ *
+ * @example
+ *   std::string msg = "Exception message";
+ *   throw SeqReaderException(msg);
+ *
+ * @example
+ *   throw SeqReaderException("Exception message");
+ */
+class SeqReaderException : public std::exception {
+ public:
+  SeqReaderException(std::string &msg) :
+    exceptionMsg(msg)
+  {}
+
+  SeqReaderException(const SeqReaderException &e) :
+    exceptionMsg(e.exceptionMsg)
+  {}
+
+  virtual const char* what() const throw() { return exceptionMsg.c_str(); }
+
+  const std::string exceptionMsg;
+};
+
+/**
+ * @brief Class for identifying the type of a given sequence file as either a
+ * FASTA or FASTQ file and return a SeqReader object inheriting from FastaReader
+ * or FastqReader, respectively.
+ */
+class SeqReader
+{
+ public:
+  SeqReader(const std::string &file);
+
+  ~SeqReader();
+
+ private:
+
+  /*
+   * Value for FASTA file type.
+   */
+  static const auto kFileTypeFasta = 1;
+
+  /*
+   * Value for FASTQ file type.
+   */
+  static const auto kFileTypeFastq = 2;
+
+  /*
+   * Size of custom buffer used to determine file type.
+   */
+  static const auto kBufferSize = 640 * 1024;
+
+  /*
+   * File read buffer.
+   */
+  ReadBuffer read_buffer_;
+
+  /*
+   * Determine the file type and return this.
+   */
+  int DetermineFileType();
+};
+
+#endif  // BIOIO_SEQ_READER_H_
---
+++ test/test_seq_reader.cc
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2015 BIO-DIKU.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#include <string>
+#include <memory>
+#include "catch.hpp"
+#include <BioIO/bioio.h>
+
+TEST_CASE("SeqReader with FASTA file", "[seq_reader]") {
+  std::string file = "test/fasta_files/test1.fasta";
+
+  SeqReader reader(file);
+
+  SECTION("First entry is read OK") {
+    REQUIRE(reader.HasNextEntry());
+
+    auto entry1 = reader.NextEntry();
+    REQUIRE(entry1->name() == "1 K#Bacteria;P#Proteobacteria");
+    REQUIRE(entry1->seq() == "ATCGUatcgu");
+
+    REQUIRE(reader.HasNextEntry());
+  }
+}
+
+TEST_CASE("SeqReader with FASTQ file", "[seq_reader]") {
+  std::string file = "test/fastq_files/test1.fastq";
+
+  SeqReader reader(file);
+
+  SECTION("First entry is read OK") {
+    static const uint8_t scores[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    const std::vector<uint8_t> v(scores, scores + sizeof(scores) / sizeof(scores[0]));
+
+    REQUIRE(reader.HasNextEntry());
+
+    auto entry1 = reader.NextEntry();
+    REQUIRE(entry1->name() == "test1");
+    REQUIRE(entry1->seq() == "ATCGUatcgu");
+    REQUIRE(entry1->scores() == v);
+
+    REQUIRE(reader.HasNextEntry());
+  }
+}
+
+TEST_CASE("SeqReader with bad file format", "[seq_reader]") {
+  string file = "temp_file";
+
+  remove(file.c_str());
+
+  ofstream output;
+  output.open(file);
+  output << " " << endl;
+  output << " A" << endl;
+  output.close();
+
+  SECTION("Throws exception") {
+    try {
+      SeqReader reader(file);
+      FAIL("SeqReader constructor did not throw expected exception");
+    }
+
+    catch (SeqReaderException& e) {
+      REQUIRE(e.exceptionMsg == "Error: File not in FASTA or FASTQ format");
+    }
+  }
+
+  remove(file.c_str());
+}
